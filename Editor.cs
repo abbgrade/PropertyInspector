aca4f652529b0bc4bc47449d052f4e343600bd1c
skampm2s <steffen.kampmann@cv-lab.de>
2014-11-25 09:21:56 +0100

    added support for interfaces

--- Editor.cs
+++ Editor.cs
@@ -5,112 +5,116 @@ using UnityEngine;
 
 namespace PropertyInspector
 {
-    [CustomEditor(typeof(MonoBehaviour), editorForChildClasses: true)]
-    public class Editor : UnityEditor.Editor
-    {
-        #region Utils
+	[CustomEditor(typeof(MonoBehaviour), editorForChildClasses: true)]
+	public class Editor : UnityEditor.Editor
+	{
+		#region Utils
 
-        private static Dictionary<string, bool> foldoutStates = new Dictionary<string, bool>();
+		private static Dictionary<string, bool> foldoutStates = new Dictionary<string, bool>();
 
-        /// <summary>
-        ///     Creates recursively EditorGUI elements for an abitrary object.
-        /// </summary>
-        private void DrawInspectedMembers(string label, object target, List<string> path, bool root = false)
-        {
-            // The unique path of member labels is used as key to save the foldout state.
-            path = new List<string>(path);
-            path.Add(label);
-            var key = string.Join("\\", path.ToArray());
+		/// <summary>
+		/// Creates recursively EditorGUI elements for an abitrary object. 
+		/// </summary>
+		private void DrawInspectedMembers(string label, object target, List<string> path, bool root = false)
+		{
+			// The unique path of member labels is used as key to save the foldout state. 
+			path = new List<string>(path);
+			path.Add(label);
+			var key = string.Join("\\", path.ToArray());
 
-            foldoutStates[key] = EditorGUILayout.Foldout(foldoutStates.ContainsKey(key) ? foldoutStates[key] : root, root ? label : string.Format("{0} ({1})", label, target.GetType().Name));
-            if (foldoutStates[key])
-            {
-                EditorGUI.indentLevel++;
+			foldoutStates[key] = EditorGUILayout.Foldout(foldoutStates.ContainsKey(key) ? foldoutStates[key] : root, root ? label : string.Format("{0} ({1})", label, target.GetType().Name));
+			if (foldoutStates[key])
+			{
+				EditorGUI.indentLevel++;
 
-                var targetType = target.GetType();
-                // Collect all members that may be displayed. Private or hidden members are left out in a later step.
-                var members = new List<System.Reflection.MemberInfo>();
-                if (!root) members.AddRange(targetType.GetFields()); // The fields of the root element is already displayed by the default inspector.
-                members.AddRange(targetType.GetProperties());
-                int visibleMemberCount = 0;
+				var targetType = target.GetType();
+				// Collect all members that may be displayed. Private or hidden members are left out
+				// in a later step.
+				var members = new List<System.Reflection.MemberInfo>();
+				if (!root) members.AddRange(targetType.GetFields()); // The fields of the root element is already displayed by the default inspector.
+				members.AddRange(targetType.GetProperties());
+				int visibleMemberCount = 0;
 
-                foreach (var member in members)
-                {
-                    string name = member.Name;
+				foreach (var member in members)
+				{
+					string name = member.Name;
 
-                    // Get the type, value and skip the member if it should not be displayed.
-                    object value;
-                    System.Type type_;
-                    if (member is System.Reflection.FieldInfo)
-                    {
-                        var field = member as System.Reflection.FieldInfo;
+					// Get the type, value and skip the member if it should not be displayed. 
+					object value;
+					System.Type type_;
+					if (member is System.Reflection.FieldInfo)
+					{
+						var field = member as System.Reflection.FieldInfo;
 
-                        // A field will be shown if it is public or has a ShowInInspector attribute and not has an HideInInspector attribute.
-                        if (!(field.IsPublic || field.GetShowInInspector()) || field.GetHideInInspector()) continue;
+						// A field will be shown if it is public or has a ShowInInspector attribute
+						// and not has an HideInInspector attribute.
+						if (!(field.IsPublic || field.GetShowInInspector()) || field.GetHideInInspector()) continue;
 
-                        value = field.GetValue(target);
-                        type_ = field.FieldType;
-                    }
-                    else if (member is System.Reflection.PropertyInfo)
-                    {
-                        var property = member as System.Reflection.PropertyInfo;
+						value = field.GetValue(target);
+						type_ = field.FieldType;
+					}
+					else if (member is System.Reflection.PropertyInfo)
+					{
+						var property = member as System.Reflection.PropertyInfo;
 
-                        // A property will be shown if it has an getter method and if it has an ShowInInspector attribute. For technical reasons is must be public.
-                        if (property.GetGetMethod(nonPublic: true) == null && property.GetGetMethod(nonPublic: false) == null) continue;
-                        if (!property.GetShowInInspector()) continue;
+						// A property will be shown if it has an getter method and if it has an
+						// ShowInInspector attribute. For technical reasons is must be public.
+						if (property.GetGetMethod(nonPublic: true) == null && property.GetGetMethod(nonPublic: false) == null) continue;
+						if (!property.GetShowInInspector()) continue;
 
-                        value = property.GetValue(target, null);
-                        type_ = property.PropertyType;
-                    }
-                    else
-                    {
-                        // This case will not appear.
-                        throw new NotImplementedException();
-                    }
+						value = property.GetValue(target, null);
+						type_ = property.PropertyType;
+					}
+					else
+					{
+						// This case will not appear. 
+						throw new NotImplementedException();
+					}
 
-                    visibleMemberCount++;
+					visibleMemberCount++;
 
-                    // Show EditorGUI element.
-                    if (type_.IsSubclassOf(typeof(MonoBehaviour)))
-                    {
-                        // Elements with an own inspector will be shown as reference.
-                        EditorGUILayout.ObjectField(name, (MonoBehaviour)value, type_, allowSceneObjects: true);
-                    }
-                    else if (value == null)
-                    {
-                        // Null elements are listed with their type.
-                        EditorGUILayout.LabelField(label: name, label2: "None (" + type_ + ")");
-                    }
-                    else if (type_.IsClass && !(value is string))
-                    {
-                        // Non primitive types are recursively displayed with a new foldout and indent level in the inspector.
-                        DrawInspectedMembers(name, value, path);
-                    }
-                    else
-                    {
-                        // Members with primitive types are shown as labels.
-                        EditorGUILayout.LabelField(label: name, label2: value.ToString());
-                    }
-                }
+					// Show EditorGUI element. 
+					if (type_.IsSubclassOf(typeof(MonoBehaviour)))
+					{
+						// Elements with an own inspector will be shown as reference. 
+						EditorGUILayout.ObjectField(name, (MonoBehaviour)value, type_, allowSceneObjects: true);
+					}
+					else if (value == null)
+					{
+						// Null elements are listed with their type. 
+						EditorGUILayout.LabelField(label: name, label2: "None (" + type_ + ")");
+					}
+					else if ((type_.IsClass || type_.IsInterface) && !(value is string))
+					{
+						// Non primitive types are recursively displayed with a new foldout and
+						// indent level in the inspector.
+						DrawInspectedMembers(name, value, path);
+					}
+					else
+					{
+						// Members with primitive types are shown as labels. 
+						EditorGUILayout.LabelField(label: name, label2: value.ToString());
+					}
+				}
 
-                if (visibleMemberCount == 0)
-                    EditorGUILayout.LabelField("No visible member.");
+				if (visibleMemberCount == 0)
+					EditorGUILayout.LabelField("No visible member.");
 
-                EditorGUI.indentLevel--;
-            }
-        }
+				EditorGUI.indentLevel--;
+			}
+		}
 
-        #endregion Utils
+		#endregion Utils
 
-        public override void OnInspectorGUI()
-        {
-            // Draw the default inspector with the serializable members.
-            DrawDefaultInspector();
+		public override void OnInspectorGUI()
+		{
+			// Draw the default inspector with the serializable members. 
+			DrawDefaultInspector();
 
-            // Draw the non serializable members recursively.
-            DrawInspectedMembers("Volatile Members", target, path: new List<string>(), root: true);
+			// Draw the non serializable members recursively. 
+			DrawInspectedMembers("Volatile Members", target, path: new List<string>(), root: true);
 
-            Repaint();
-        }
-    }
+			Repaint();
+		}
+	}
 }
